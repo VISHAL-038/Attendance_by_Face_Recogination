5c8b0f3668137164f8d7c7164b902b7267e92b1c
vishal-038 <vk067414@gmail.com>
2024-09-12 09:17:12 +0530

    "model is developed"

---
+++ myenv/Lib/site-packages/streamlit_autorefresh/frontend/build/precache-manifest.410d6a3f2e708cfa56b6d9e4ad8a186d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7ad16e8ca18bf1d1158b84b91a3a4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b5a65c74878b2c0b3e0",
+    "url": "./static/js/2.501b5008.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.501b5008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "becb381c518f9a979a61",
+    "url": "./static/js/main.d7c3a224.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
---
+++ myenv/Lib/site-packages/streamlit_autorefresh/frontend/build/precache-manifest.fb4637176de96fc72e9a091be53dbacf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7604bbfa34253ad9b424a95d47c2c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "342a8900ef0281190ad5",
+    "url": "./static/js/2.b15cb7c9.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.b15cb7c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6ad204c1b0f6b3b292",
+    "url": "./static/js/main.ec9ffac2.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
